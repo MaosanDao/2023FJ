c982681fd1e7847a976d15ea45664c67c3a3de5d
Vangelis <460977141@qq.com>
2023-03-10 17:50:23 +0800

    新增主界面以及更新gradle版本

---
+++ app/src/main/java/com/vangelis/fj2023/MainActivity.java
@@ -0,0 +1,22 @@
+package com.vangelis.fj2023;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Function: #todo
+ * Created on 2023/3/10.
+ *
+ * @author Wangpei
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
