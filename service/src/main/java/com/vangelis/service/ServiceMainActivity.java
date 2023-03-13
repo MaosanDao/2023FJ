08919e4c914d38ab8cdb8e53ccfc5a62092e3a85
vangelis <onlywangpei@qq.com>
2023-03-13 16:41:13 +0800

    新增service小MODULE

---
+++ service/src/main/java/com/vangelis/service/ServiceMainActivity.java
@@ -0,0 +1,14 @@
+package com.vangelis.service;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ServiceMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.service_activity_main);
+    }
+}
